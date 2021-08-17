e022e6ac62f6181bc3205012574a1225556f90ad
luka <lukacerrutti2002@gmail.com>
2021-08-17 01:42:01 -0300

    Entrega final.

--- js/script.js
+++ js/script.js
@@ -3,13 +3,16 @@ new WOW().init();
 let logo = document.getElementById('logo'); 
 let nav = document.getElementById('navbar');  
 const root = document.querySelector(':root');
-
-if(document.cookie.charAt(document.cookie.length-1) == 1) root.classList.toggle('dark');
-else root.classList.remove('dark');
+const page = window.location.pathname.split("/").pop().replace('.html', '');
+let theme = localStorage.getItem("holotheme");
+if(theme == 1){   
+    root.classList.toggle('dark');
+} else {
+    root.classList.remove('dark'); 
+}
 
 window.onload = function() {
     const width  = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    
     if (width >= 992) {
         logo.style.height = '64px';
     } else{
@@ -17,36 +20,51 @@ window.onload = function() {
     }
     
     nav.classList.add("navbar--noborder");
+
+    if(page != 'index' && page != ''){
+        nav.classList.add("bg-light--bg");
+
+        if(page != 'product') nav.classList.remove("navbar--noborder");
+        
+        var buttons = document.getElementsByClassName("nav-link");
+        for(i = 0; i < buttons.length; i++) {buttons[i].className += " nav-link--scroll";}
+    }
+
+    var temp = location.hash;
+    if(temp){
+        setTimeout(function(){ location.hash = temp.split('?')[0]; }, 750);
+    }
 }
 
 window.onscroll = () => {
-    const width  = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    const width  = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth; 
     
     if (width >= 992) {
         if (window.scrollY > width/24) {
-            logo.style.height = '40px';
+            logo.style.height = '40px';   
             nav.classList.add("bg-light--bg");
-            nav.classList.remove("navbar--noborder");
-            
+            nav.classList.remove("navbar--noborder");    
             var buttons = document.getElementsByClassName("nav-link");
             for(i = 0; i < buttons.length; i++) {buttons[i].className += " nav-link--scroll";}
         } else{
             logo.style.height = '64px';
-            nav.classList.remove("bg-light--bg");
-            nav.classList.add("navbar--noborder");
-
-            var buttons = document.getElementsByClassName("nav-link");
+            if(page == 'product') nav.classList.add("navbar--noborder");
+            if(page == 'index' || page == ''){
+                nav.classList.remove("bg-light--bg");
+                nav.classList.add("navbar--noborder");
+                var buttons = document.getElementsByClassName("nav-link");
             for(i = 0; i < buttons.length; i++) {buttons[i].classList.remove("nav-link--scroll");}
+            }
         }
     }
 };  
 
 document.querySelector(".btn-toggle").addEventListener("click", function() {
     if(!root.classList.contains('dark')){
-        document.cookie = "darkmode=1; SameSite=None; Secure"; 
-        root.classList.toggle('dark'); 
+        window.localStorage.setItem('holotheme', 1)
+        root.classList.add('dark'); 
     } else {
-        document.cookie = "darkmode=0; SameSite=None; Secure"; 
+        window.localStorage.setItem('holotheme', 0)
         root.classList.remove('dark');
     }
 });
